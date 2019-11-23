6de64bc123b6653a5a9043b7b0524c5b486618c5
psute <psutek@gmail.com>
2019-11-23 13:04:08 +0100

    Add new tests

--- src/test/java/page/objects/LoginPage.java
+++ src/test/java/page/objects/LoginPage.java
@@ -7,11 +7,6 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
-    public LoginPage(WebDriver driver) {
-        this.driver = driver;
-        PageFactory.initElements(driver, this);
-    }
-
     @FindBy(name = "username")
     private WebElement usernameField;
 
@@ -24,27 +19,36 @@ public class LoginPage {
     @FindBy(css = "#Content ul[class='messages'] li")
     private WebElement messageLabel;
 
-    @FindBy (xpath = "//LI[text()='You must sign on before attempting to check out.  Please sign on and try checking out again.']")
+    @FindBy(css = "area[alt='Fish']")
+    private WebElement btnFishImage;
 
     private WebDriver driver;
 
-    public void typeIntoUserNameField(String username){
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void typeIntoUserNameField(String username) {
         usernameField.clear();
         usernameField.sendKeys(username);
     }
 
-    public void typeIntoPasswordField(String password){
+    public void typeIntoPasswordField(String password) {
         passwordField.clear();
         passwordField.sendKeys(password);
     }
 
-    public void clickOnLoginButton(){
+    public void clickOnLoginButton() {
         signOnButton.click();
     }
 
-    public String getWarningMessage(){
+    public String getWarningMessage() {
         String warningText = messageLabel.getText();
         return warningText;
     }
 
+    public void clickOnFishImageButton() {
+        btnFishImage.click();
+    }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,7 +17,7 @@ public class TestBase {
 
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+        driver.manage().window().maximize();
         driver.navigate().to("http://przyklady.javastart.pl/jpetstore/");
     }
 
