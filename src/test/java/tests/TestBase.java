6fc2a8e766dd62d2c4cfb0975a3f96b1404ac253
psute <psutek@gmail.com>
2019-11-23 16:21:15 +0100

    Add new tests

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,22 +1,19 @@
 package tests;
 
 import driver.manager.DriverManager;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.util.concurrent.TimeUnit;
+
 
 public class TestBase {
 
-    public WebDriver driver;
-
     @BeforeMethod
     public void beforeTest() {
-        driver = DriverManager.getWebDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.navigate().to("http://przyklady.javastart.pl/jpetstore/");
+        DriverManager.getWebDriver();
+        DriverUtilis.setInitialConfiguration();
+        DriverUtilis.navigateToPage("http://przyklady.javastart.pl/jpetstore/");
+
     }
 
     @AfterMethod
